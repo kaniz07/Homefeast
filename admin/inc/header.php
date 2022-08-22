57cc7d51f18cbfeca4cff1c27cc16aa16b60bc8e
kaniz07 <kanizety789@gmail.com>
2022-08-22 05:02:36 -1200

    admin page for frontend created

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -20,20 +20,20 @@
 	<link rel="shortcut icon" href="images/favicon.png">
 
 	<!-- CSS -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="js/isotope/isotope.css">
-	<link rel="stylesheet" href="js/flexslider/flexslider.css">
-	<link rel="stylesheet" href="js/owl-carousel/owl.carousel.css">
-	<link rel="stylesheet" href="js/owl-carousel/owl.theme.css">
-	<link rel="stylesheet" href="js/owl-carousel/owl.transitions.css">
-	<link rel="stylesheet" href="js/superfish/css/megafish.css" media="screen">
-	<link rel="stylesheet" href="js/superfish/css/superfish.css" media="screen">
-	<link rel="stylesheet" href="js/pikaday/pikaday.css">
-	<link rel="stylesheet" href="css/settings-panel.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/light.css">
-	<link rel="stylesheet" href="css/responsive.css">
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../js/isotope/isotope.css">
+	<link rel="stylesheet" href="../js/flexslider/flexslider.css">
+	<link rel="stylesheet" href="../js/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="../js/owl-carousel/owl.theme.css">
+	<link rel="stylesheet" href="../js/owl-carousel/owl.transitions.css">
+	<link rel="stylesheet" href="../js/superfish/css/megafish.css" media="screen">
+	<link rel="stylesheet" href="../js/superfish/css/superfish.css" media="screen">
+	<link rel="stylesheet" href="../js/pikaday/pikaday.css">
+	<link rel="stylesheet" href="../css/settings-panel.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/light.css">
+	<link rel="stylesheet" href="../css/responsive.css">
 
 	<!-- JS Font Script -->
 	<script src="http://use.edgefonts.net/bebas-neue.js"></script>
@@ -53,11 +53,10 @@
 <div id="wrapper" class="wrapper">
 	<!-- HEADER -->
 	<header id="header2">
-
 	<div class="container">
 			<div class="row">
 				<div class="col-md-3 col-xs-5 logo">
-					<a href="http://[::1]/cishop/admin"><img src="http://[::1]/cishop/assets/images/logo.png" class="img-responsive" alt=""/></a>
+					<a href="http://localhost/Homefeast/admin"><img src="http://[::1]/cishop/assets/images/logo.png" class="img-responsive" alt=""/></a>
 				</div>
 				<div class="col-md-9 col-xs-7">
 					<div class="top-bar">
---
+++ admin/inc/nav.php
@@ -0,0 +1,112 @@
+<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-xs-5 logo">
+					<a href="#"><img src="#" class="img-responsive" alt=""/></a>
+				</div>
+				<div class="col-md-9 col-xs-7">
+					<div class="top-bar">
+						
+					</div>
+				</div>
+			</div>
+			<div class="menu-wrap">
+				<div id="mobnav-btn">Menu <i class="fa fa-bars"></i></div>
+				<ul class="sf-menu">
+					<li>
+						<a href="index.php">Dashboard</a>
+					</li>
+					<li>
+						<a href="#">Categories</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="categories.php">View categories</a></li>
+							<li><a href="addcategory.php">Add category</a></li>
+					
+						</ul>
+					</li>
+
+                    <li>
+						<a href="#">Food Item</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="items.php">View Items</a></li>
+							<li><a href="additem.php">Add Items</a></li>
+					
+						</ul>
+					</li>
+
+
+					
+                    <li>
+						<a href="#">Orders</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="orders.php">View Orders</a></li>
+							
+					
+						</ul>
+					</li>
+
+                    <li>
+						<a href="#">My Account</a>
+						<div class="mobnav-subarrow"><i class="fa fa-plus"></i></div>
+						<ul>
+							<li><a href="#">Edit Profile</a></li>
+							<li><a href="logout.php">Logout</a></li>
+						</ul>
+					</li>
+
+
+					
+				</ul>
+				<div class="header-xtra">
+					<div class="s-cart">
+						<div class="sc-ico"><i class="fa fa-shopping-cart"></i><em>2</em></div>
+
+						<div class="cart-info">
+							<small>You have <em class="highlight">#</em> in your shopping bag</small>
+							<br>
+							<br>
+							<div class="ci-item">
+								<img src="images/shop/2.jpg" width="70" alt=""/>
+								<div class="ci-item-info">
+									<h5><a href="#">Demo 1</a></h5>
+									<p>demo price</p>
+									<div class="ci-edit">
+										<a href="#" class="edit fa fa-edit"></a>
+										<a href="#" class="edit fa fa-trash"></a>
+									</div>
+								</div>
+							</div>
+							<div class="ci-item">
+								<img src="images/shop/8.jpg" width="70" alt=""/>
+								<div class="ci-item-info">
+									<h5><a href="./single-product.html">item 1</a></h5>
+									<p>price 1</p>
+									<div class="ci-edit">
+										<a href="#" class="edit fa fa-edit"></a>
+										<a href="#" class="edit fa fa-trash"></a>
+									</div>
+								</div>
+							</div>
+							<div class="ci-total">total price</div>
+							<div class="cart-btn">
+								<a href="#">View Bag</a>
+								<a href="#">Checkout</a>
+							</div>
+						</div>
+					</div>
+					<div class="s-search">
+						<div class="ss-ico"><i class="fa fa-search"></i></div>
+						<div class="search-block">
+							<div class="ssc-inner">
+								<form>
+									<input type="text" placeholder="Type Search text here...">
+									<button type="submit"><i class="fa fa-search"></i></button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
