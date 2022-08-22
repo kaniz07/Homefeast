54af764bfdde7440ea8921031a622cd6db4ed73d
kaniz07 <kanizety789@gmail.com>
2022-08-21 19:04:40 -1200

    customer account and checkout page created

---
+++ myaccount.php
@@ -0,0 +1,105 @@
+<?php
+include('inc/header.php');
+include('inc/nav.php')
+?>
+
+	
+	<!-- SHOP CONTENT -->
+	<section id="content">
+		<div class="content-blog content-account">
+			<div class="container">
+				<div class="row">
+					<div class="page_header text-center">
+						<h2>My Account</h2>
+					</div>
+					<div class="col-md-12">
+
+			<h3>Recent Orders</h3>
+			<br>
+			<table class="cart-table account-table table table-bordered">
+				<thead>
+					<tr>
+						<th>Order</th>
+						<th>Date</th>
+						<th>Status</th>
+						<th>Total</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>
+							003
+						</td>
+						<td>
+							June 15,2022
+						</td>
+						<td>
+							Delivered			
+						</td>
+						<td>
+							demo price for 4 items				
+						</td>
+						<td>
+							<a href="#">View</a>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							002
+						</td>
+						<td>
+							June 2, 2022
+						</td>
+						<td>
+							Delivered			
+						</td>
+						<td>
+							demo price for 2 items				
+						</td>
+						<td>
+							<a href="#">View</a>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							001
+						</td>
+						<td>
+							March 23, 2022
+						</td>
+						<td>
+							Delivered			
+						</td>
+						<td>
+							demo price for 14 items				
+						</td>
+						<td>
+							<a href="#">View</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>		
+
+			<br>
+			<br>
+			<br>
+
+			
+
+
+
+			</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<div class="clearfix space70"></div>
+    <?php
+
+include('inc/footer.php')
+
+?>
